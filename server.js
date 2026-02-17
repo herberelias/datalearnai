a13b3846efe0ee739091bc9ed3160b54737e7be7
Herber Elias <programador03@DistSalv.onmicrosoft.com>
2026-02-17 16:25:07 -0600

    fix: Update route to /api/chatbotmysql for mobile app compatibility

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/chatbot', chatbotRoutes);
+app.use('/api/chatbotmysql', chatbotRoutes);
 
 // Root route
 app.get('/', (req, res) => {
